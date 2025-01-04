8d0bb08ed92b787089799f57cc4fd76dda7afd2b
Joseph Petrillo <jpetrillo119@gmail.com>
2025-01-04 17:03:05 -0500

    fix width

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex grow flex-col items-center justify-center px-8 pt-12 pb-[calc(3rem+5vh)]">
-      <div className="w-full max-w-sm text-balance">
+      <div className="w-full max-w-md text-balance">
         <p className="mb-3 text-2xl font-bold text-gray-950">404 Not Found</p>
         <p className="mb-6 text-gray-700">
           Sorry, the page you are trying to load does not exist.
